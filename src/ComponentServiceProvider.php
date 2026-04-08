d7cf5f32516aa3240cfd82ce3472764dad084379
anthonymacs <157227172+anthonymacs@users.noreply.github.com>
2026-04-08 12:29:04 +0700

    updated

---
+++ src/ComponentServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace anthonymacs\LaravelComponents;
+
+use Illuminate\Support\ServiceProvider;
+
+class ComponentServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        // Load views from the package
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'components');
+
+        // Publish views so users can override them
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/components'),
+        ], 'components-views');
+    }
+
+    public function register(): void
+    {
+        //
+    }
+}
